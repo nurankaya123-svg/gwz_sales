c02dfbba43208f8c661ea9467393cd554a3e730d
nurankaya123-svg <nurankaya123@gmail.com>
2026-03-25 16:23:16 +0300

    düzenleme

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,8 +1,8 @@
 SELECT
-  date_date,
+ date_date,
   SUM(turnover) AS daily_turnover,
   SUM(purchase_cost) AS daily_purchase_cost,
-  SUM(turnover) - SUM(purchase_cost) AS daily_gross_margin
+  SUM(turnover) - SUM(purchase_cost) AS ddaily_gross_margin
 FROM `course14.gwz_sales`
 GROUP BY date_date
 ORDER BY date_date DESC
