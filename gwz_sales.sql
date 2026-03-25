1a7ddd8d503d48027fc51dfa1a3f48d3d1c6c23f
nurankaya123-svg <nurankaya123@gmail.com>
2026-03-25 16:41:30 +0300

    ayılar 2 ondalık basamağa yuvarlandı

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,8 +1,8 @@
 SELECT
- date_date,
-  SUM(turnover) AS daily_turnover,
-  SUM(purchase_cost) AS daily_purchase_cost,
-  SUM(turnover) - SUM(purchase_cost) AS ddaily_gross_margin
+    date_date,
+    ROUND(SUM(turnover), 2) AS daily_turnover,
+    ROUND(SUM(purchase_cost), 2) AS daily_purchase_cost,
+    ROUND(SUM(turnover) - SUM(purchase_cost), 2) AS daily_gross_margin
 FROM `course14.gwz_sales`
 GROUP BY date_date
 ORDER BY date_date DESC
