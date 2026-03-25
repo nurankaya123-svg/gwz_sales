a61938f4426e4ac469dbe99d86321c5f999bc45b
nurankaya123-svg <nurankaya123@gmail.com>
2026-03-25 14:28:47 +0300

    Update gwz_sales.sql
    Satın alma maliyeti eklendi

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,6 +1,8 @@
 SELECT
   date_date,
-  SUM(turnover) AS daily_turnover
-FROM `course_14.gwz_sales`
+  SUM(turnover) AS daily_turnover,
+  SUM(purchase_cost) AS daily_purchase_cost,
+  SUM(turnover) - SUM(purchase_cost) AS daily_gross_margin
+FROM `course14.gwz_sales`
 GROUP BY date_date
 ORDER BY date_date DESC
